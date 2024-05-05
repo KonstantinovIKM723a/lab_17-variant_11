67b202854bd44531d98d1d0b3bf5f5e2d26f69de
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-05-05 16:05:59 +0300

    Add project files.

---
+++ lab_17-variant_11/lab_17-variant_11.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+
+
+class File {
+    string name;
+    int size;
+    string creationDate;
+    int assignCount;
+public:
+    File() : name(""), size(0), creationDate(""), assignCount(0) {}
+    File(string n, int s, string c, int a) : name(n), size(s), creationDate(c), assignCount(a) {}
+    string getName() const { return name; }
+    int getSize() const { return size; }
+    string getCreationDate() const { return creationDate; }
+    int getAssignCount() const { return assignCount; }
+    void showFileInfo();
+};
+
+void File::showFileInfo() {
+    cout << "Файл \"" << name << "\":" << endl;
+    cout << "Розмір: " << size << endl;
+    cout << "Дата створення: " << creationDate << endl;
+    cout << "Кількість звернень: " << assignCount << endl;
+    cout << endl;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    
+}
